d09da5fc6a5577c94e8c1c536f310ab880e3224f
Rednigo <rostyslav.baranovskyi@lnu.edu.ua>
2022-11-03 09:57:03 +0200

    Трохи пописав

--- server.py
+++ server.py
@@ -4,39 +4,42 @@ from multiprocessing import Process
 from flask import Flask, request
 
 app = Flask(__name__)
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        return
+@app.route("/users/<user_id>", methods=['GET', 'POST'])
+def user(user_id):
+   #if request.method == 'GET':
 
+   if request.method == 'POST':
+       data = request.form.get("some_data")
+
+#Щоб запустити прописати в консолі: flask --app server run --host=0.0.0.0
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
-class Handler(server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-        body_length = int(self.headers['content-length'])
-        request_body_json_string = self.rfile.read(body_length).decode('utf-8')
-        
-        # Printing  some info to the server console
-        print('Server on port ' + str(self.server.server_port) + ' - request body: ' + request_body_json_string)
-        
-        json_data_obj = json.loads(request_body_json_string)
-        json_data_obj['SEEN_BY_THE_SERVER'] = 'Yes'
-        
-        # Sending data to the client
-        self.wfile.write(bytes(json.dumps(json_data_obj), 'utf-8'))
-
-def start_server(server_address):
-    my_server = server.ThreadingHTTPServer(server_address, Handler)
-    print(str(server_address) + ' Waiting for POST requests...')
-    my_server.serve_forever()
-
-def start_local_server_on_port(port):
-    p = Process(target=start_server, args=(('127.0.0.1', port),))
-    p.start()
-
-if(__name__ == '__main__'):
-    start_local_server_on_port(8011)
-    start_local_server_on_port(8012)
+# class Handler(server.BaseHTTPRequestHandler):
+#     def do_POST(self):
+#         self.send_response(200)
+#         self.end_headers()
+#         body_length = int(self.headers['content-length'])
+#         request_body_json_string = self.rfile.read(body_length).decode('utf-8')
+#
+#         # Printing  some info to the server console
+#         print('Server on port ' + str(self.server.server_port) + ' - request body: ' + request_body_json_string)
+#
+#         json_data_obj = json.loads(request_body_json_string)
+#         json_data_obj['SEEN_BY_THE_SERVER'] = 'Yes'
+#
+#         # Sending data to the client
+#         self.wfile.write(bytes(json.dumps(json_data_obj), 'utf-8'))
+#
+# def start_server(server_address):
+#     my_server = server.ThreadingHTTPServer(server_address, Handler)
+#     print(str(server_address) + ' Waiting for POST requests...')
+#     my_server.serve_forever()
+#
+# def start_local_server_on_port(port):
+#     p = Process(target=start_server, args=(('127.0.0.1', port),))
+#     p.start()
+#
+# if(__name__ == '__main__'):
+#     start_local_server_on_port(8011)
+#     start_local_server_on_port(8012)
